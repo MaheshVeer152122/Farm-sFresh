978e9be77902cd935753ea817df224a87116ddc6
MaheshVeer152122 <110464907+MaheshVeer152122@users.noreply.github.com>
2023-08-30 16:09:02 +0530

    Uploading Spring Boot starter application
    Adding skeleton of project made using STS

---
+++ FrarmsFresh/src/main/java/com/fresh/FrarmsFreshApplication.java
@@ -0,0 +1,13 @@
+package com.fresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrarmsFreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrarmsFreshApplication.class, args);
+	}
+
+}
---
+++ FrarmsFresh/src/test/java/com/fresh/FrarmsFreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrarmsFreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
