f4a9f684a178daeda852e79eb683d26947b38931
MaheshVeer152122 <110464907+MaheshVeer152122@users.noreply.github.com>
2023-08-31 14:53:26 +0530

    Customized Exception hadler
    this handle will handle exception related to Resource not found.

---
+++ FrarmsFresh/src/main/java/com/fresh/custom_exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.fresh.custom_exceptions;
+
+@SuppressWarnings("serial")
+public class ResourceNotFoundException extends RuntimeException {
+	public ResourceNotFoundException(String mesg) {
+		super(mesg);
+	}
+}
